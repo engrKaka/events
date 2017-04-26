f27a48ab8f9e0aaea1c2748f9d3471bdb9a2c3fb
Haris Mansoor Khan <pashmantech@gmail.com>
2017-04-26 23:51:44 +0500

    setting inital commit

---
+++ resources/views/dashboard/settings/home.blade.php
@@ -0,0 +1,109 @@
+@extends('dashboard.layouts.app')
+@section('header')
+    <section class="content-header hidden-xs">
+        <div class="header-icon">
+            <i class="ti-home"></i>
+        </div>
+        <div class="header-title">
+            <h1>Settings</h1>
+            <small>Manage Admins.</small>
+            @include('dashboard.partials.quick-links')
+            <ol class="breadcrumb">
+                <li><a href="{{ url('/') }}"><i class="pe-7s-home"></i> Home</a></li>
+                <li><a href="{{ url('/dashboard') }}"> Dashboard</a></li>
+                <li class="active">Home Settings</li>
+            </ol>
+        </div>
+    </section>
+@endsection
+@section('content')
+    <div class="row">
+        <div class="col-sm-12 " >
+            <div class="panel panel-bd ">
+                <div class="panel-heading ">
+                    <div  class="panel-title col-sm-10">
+                        <h4>Home Page Settings</h4>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="panel-body">
+
+                    {!! Form::open(['action'=>['Dashboard\SettingsController@update','slug'=>'home_page'],'method'=>'patch','class'=>'form-horizontal','enctype'=>'multipart/form-data']) !!}
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2">
+                            <h3 class="text-center">Home Page Settings</h3>
+                            <hr>
+                            @foreach($Settings('home_page') as $key=>$setting)
+                                <div class="form-group {{ $errors->has($key) ? ' has-error' : '' }}">
+                                    {!! Form::label($key, $setting->title, ['class' => 'col-sm-2 control-label'])  !!}
+                                    <div class="col-sm-9">
+                                        @if($key=='slider_background')
+                                            <input type="file" class="input-slider-background" name="slider_background">
+                                        @elseif($key=='meta_description' || $key=='slider_message')
+                                            {!! Form::textarea($key,$value= $setting->value, $attributes = ['class'=>'form-control','placeholder'=>$setting->title])  !!}
+                                        @else
+                                            {!! Form::text($key,$value= $setting->value, $attributes = ['class'=>'form-control','placeholder'=>$setting->title])  !!}
+                                        @endif
+                                        @if ($errors->has($key))
+                                            <span class="help-block">
+                                            <strong>{{ $errors->first($key) }}</strong>
+                                        </span>
+                                        @endif
+                                        @if($key=='slider_background')
+                                            <div class="slider-background-preview">
+                                                @if(!empty(($setting->value)) && file_exists(public_path('images/'.$setting->value)))
+                                                    <img style="max-width: 200px;" class="img-thumbnail" src="{{ asset('images/'.$setting->value) }}" alt="{{ $setting->title }}">
+                                                @else
+                                                    <img style="max-width: 200px;" class="img-thumbnail" src="{{ asset('images/not-found.jpg') }}"  alt="{{ $setting->title }}">
+                                                @endif
+                                            </div>
+                                        @endif
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2">
+                                <span class="pull-right">
+                                     <button type="reset" class="btn btn-default">Cancel</button>
+                                     &nbsp;
+                                     <button type="submit" class="btn btn-info ">Update</button>
+                                </span>
+                        </div>
+                    </div>
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
+@section('scripts')
+    <script type="text/javascript">
+        $(function() {
+
+            function readURL(input,callback) {
+                if (input.files && input.files.length>0) {
+                    for (var i=0;i<input.files.length;i++){
+                        var reader = new FileReader();
+                        reader.onload = function (e) {
+                            callback(e);
+                        };
+                        reader.readAsDataURL(input.files[i]);
+                    }
+                }
+            }
+
+            $(".input-slider-background").change(function () {
+                $('.slider-background-preview').html('');
+                readURL(this,function (e) {
+                    $('.slider-background-preview').append('<img style="width: 120px; height: 120px;" src="'+ e.target.result +'" id="image_upload_preview" class="img-responsive img-thumbnail">')
+                });
+            });
+
+
+        });
+    </script>
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,61 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'HomeController@index');
+Auth::routes();
+
+//Route::get('/home', 'HomeController@index');
+Route::group(['prefix'=>'dashboard','namespace'=>'Dashboard','middleware'=>['auth']],function (){
+    Route::get('/','HomeController@index');
+    Route::get('contact','HomeController@showContactForm');
+    Route::post('contact','HomeController@contact');
+    Route::resource('category','CategoryController');
+    Route::resource('development','DevelopmentController');
+    Route::resource('location','LocationController');
+    Route::resource('lifestyle','LifeStyleController');
+    Route::resource('area','AreaController');
+    Route::resource('amenities','AmenitiesController');
+    Route::delete('property/image/{id}','PropertyController@deleteLogo');
+    Route::get('property/sale','PropertyController@sale');
+    Route::get('property/holiday','PropertyController@holiday');
+    Route::get('property/rental','PropertyController@rent');
+    Route::get('property/offers','PropertyController@offers');
+    Route::resource('property','PropertyController');
+    Route::resource('images','ImageController');
+    Route::delete('news/image/{id}','NewsController@deleteImage');
+    Route::resource('news','NewsController');
+    Route::resource('tags','TagsController');
+    Route::resource('subscriber','SubscriberController');
+    Route::post('enquiry/reply','EnquiryController@reply');
+    Route::resource('enquiry','EnquiryController');
+    Route::post('messages/reply','MessagesController@reply');
+    Route::resource('messages','MessagesController');
+    Route::resource('comments','NewsCommentsController');
+    Route::resource('testimonial','TestimonialController');
+    Route::resource('page','PagesController');
+    Route::resource('user','UserController');
+    Route::resource('mail','EmailController');
+    Route::resource('menu','MenuController');
+    Route::resource('events','EventsController');
+    Route::resource('services','ServicesController');
+    Route::get('settings/{slug}','SettingsController@index');
+    Route::patch('settings/{slug}','SettingsController@update');
+    Route::get('analytic/countries','AnalyticController@countries');
+    Route::get('analytic/browsers','AnalyticController@browsers');
+    Route::get('analytic/os','AnalyticController@os');
+    Route::get('analytic/sources','AnalyticController@sources');
+    Route::get('analytic/mobile','AnalyticController@mobile');
+    Route::get('analytic/sites','AnalyticController@sites');
+    Route::get('analytic/keywords','AnalyticController@keywords');
+    Route::get('analytic/landing_pages','AnalyticController@landing_pages');
+    Route::get('analytic/exit_pages','AnalyticController@exit_pages');
+    Route::get('analytic/bounces','AnalyticController@bounces');
+    Route::get('users/interesting','UserController@interesting');
+    Route::get('currency','CurrencyController@index');
+    Route::post('currency','CurrencyController@store');
+    Route::post('todo','HomeController@todo');
+    Route::delete('users/interesting/{id}','UserController@destroy_interesting');
+
+
 });
+
